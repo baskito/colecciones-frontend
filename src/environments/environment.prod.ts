5aae5c6704c5a0547225866da2744fec21f495fb
Iker Bascones <ibasko@gmail.com>
2023-09-29 14:34:27 +0200

    Modify environment url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@
 
 export const environment = {
   production: true,
-  // base_url: 'https://colecciones-qa5t-dev.fl0.io/api'
-  base_url: 'http://localhost:3000/api'
+  base_url: 'https://colecciones-qa5t-dev.fl0.io/api'
+  // base_url: 'http://localhost:3000/api'
 };
